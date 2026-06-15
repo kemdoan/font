0bd3ab6cbedc755bfa1ce3e829e786c642016458
Kem Đoàn <kemdoan@gmail.com>
2026-06-16 00:30:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,50 +12,49 @@ const allFonts = [
   { name: 'KD Hongkong',        cat: 'khac',     type: 'traphi', img: 'images/kdhongkong.jpg',       link: '1qG0N-tU6mKRC5CZxXQqMO9Q94WcfMtUl', style: '3 style' },
   { name: 'KD Valery',          cat: 'cuoi',     type: 'traphi',  img: 'images/kdvalery.jpg',        link: '1CiQpd4ugE0FuV1Nmo8APvQ27LxR2Eufa' },
   { name: 'KD Doan Nguyen',     cat: 'khac',     type: 'traphi',  img: 'images/kddoannguyen.jpg',    link: '1d3eh5Ge2q_-kgiyv8OHngvXVDOerk0tO', style: '2 style' },
-  { name: 'KD Kaelyna Script',  cat: 'cuoi',     type: 'mienphi', img: 'images/kdkaelynascript.jpg', link: '1OG0Aq95FJM4gxVVzO37Tw7-aPOugEpbl' },
+  { name: 'KD Kaelyna Script', cat: 'cuoi', type: 'mienphi', img: 'images/kdkaelynascript.jpg',      link: '1OG0Aq95FJM4gxVVzO37Tw7-aPOugEpbl', vipLink: '1qbrtxHkvMby1ZX5tGA3uKLn-7yO0GvjG' },
   { name: 'KD Huees',           cat: 'khac',     type: 'traphi',  img: 'images/kdhuees.jpg',         link: '1qsXoKu7nm_DGpcyAla6gsZFw88hmuLai' },
   { name: 'KD Kem Doan',        cat: 'khac',     type: 'traphi',  img: 'images/kdkemdoan.jpg',       link: '17j_BgCy0kTpTpNDBRv2Qqpu1guhkY-KD' },
   { name: 'KD Lemaire',         cat: 'cuoi',     type: 'traphi',  img: 'images/kdlemaire.jpg',       link: '1j0tWnOrhLm5S5oTQiUyiM6TPKXiSvtlQ' },
   { name: 'KD Wedding Dress',   cat: 'cuoi',     type: 'traphi',  img: 'images/kdweddingdress.jpg',  link: '18BGqUzfqFxTfCgPBcIpiZ3S4KshrZ_vZ' },
   { name: 'KD Withlove',        cat: ['viettay', 'cuoi'], type: 'traphi',  img: 'images/kdwithlove.jpg',      link: '1gM8WdO1eWV1oaHgrWRfeKrL8m8vKc5T8' },
-  { name: 'KD Hidayatullah',    cat: 'khac',     type: 'mienphi', img: 'images/kdhidayatullah.jpg',  link: '1Ezwn8hE0hfGjUQq3y64dqlUXMAdtV4qx' },
+  { name: 'KD Hidayatullah',    cat: 'khac',     type: 'mienphi', img: 'images/kdhidayatullah.jpg',  link: '1Ezwn8hE0hfGjUQq3y64dqlUXMAdtV4qx', vipLink: '1rOW_GWyz8XXQ42YddwnACe9lcCEMH7SZ' },
   { name: 'KD Against',         cat: 'cuoi',     type: 'traphi',  img: 'images/kdagainst.jpg',       link: '1lE_kE3c8B5WVh3C93ZqzDXkTkbPY9KHQ' },
   { name: 'KD Agilera',         cat: 'cuoi',     type: 'traphi',  img: 'images/kdagilera.jpg',       link: '1gaF6rmeY_KrE6J05taeCFdAEDEZ9_Pus' },
-  { name: 'KD Moon Light',      cat: 'cuoi',     type: 'mienphi', img: 'images/kdmoonlight.jpg',     link: '1bcmTHuHTuxnNZJKddBe-iH6Ac-HftnyB' },
+  { name: 'KD Moon Light',      cat: 'cuoi',     type: 'mienphi', img: 'images/kdmoonlight.jpg',     link: '1bcmTHuHTuxnNZJKddBe-iH6Ac-HftnyB', vipLink: '1-ZSvC6fX2_lnoABGvEl-xxF3BA_-owpe' },
   { name: 'KD Type Ultimate',   cat: 'cuoi',     type: 'traphi',  img: 'images/kdtypeultimate.jpg',  link: '1WwUvBOAn2IMXl5feX1Wp0sLfcZ_MWuf-' },
-  { name: 'KD Arturo',          cat: 'khac',     type: 'mienphi', img: 'images/kdarturo.jpg',        link: '1QSZcUMrlfWeBb-2PDdyASROMRXGHfP-2' },
+  { name: 'KD Arturo',          cat: 'khac',     type: 'mienphi', img: 'images/kdarturo.jpg',        link: '1QSZcUMrlfWeBb-2PDdyASROMRXGHfP-2', vipLink: '1qCcQttNdU6s2Ol4FJ2Rva5BCkFD6-Fe5' },
   { name: 'KD Broisther',       cat: 'cuoi',     type: 'traphi',  img: 'images/kdbroisther.jpg',     link: '1bz0XpKMtIiYIliiQa8kZjNnamGSfCkpT' },
-  { name: 'KD Aureligena Script',cat: 'cuoi',     type: 'mienphi', img: 'images/kdaureligenascript.jpg', link: '1zKickXBrhIsYtrkYKLH8ecB8aPqERWBP' },
+  { name: 'KD Aureligena Script',cat: 'cuoi',     type: 'mienphi', img: 'images/kdaureligenascript.jpg', link: '1zKickXBrhIsYtrkYKLH8ecB8aPqERWBP', vipLink: '1kOXDPehHdNbTLvDcjBMUnSoejag5DZik' },
   { name: 'KD Bailey',          cat: 'khac',     type: 'traphi',  img: 'images/kdbailey.jpg',        link: '1OV1u7XcwelsEN2ZkQNXYTzTYcXu-tp-8' },
   { name: 'KD Bakar',           cat: 'khac',     type: 'traphi',  img: 'images/kdbakar.jpg',         link: '13FsHqZhli67bCoi8MnYJSxhpx0HYbExq' },
-  { name: 'KA Adidas Pro',      cat: 'khac',     type: 'mienphi', img: 'images/kdadidaspro.jpg',     link: '1Y8LTc1LL_xRb4_bOjPv9-her0aJE6zYN' },
+  { name: 'KD Adidas Pro',      cat: 'khac',     type: 'mienphi', img: 'images/kdadidaspro.jpg',     link: '1Y8LTc1LL_xRb4_bOjPv9-her0aJE6zYN', vipLink: '1VvUOncNXp56ZY4MxKyZu82uDks7WDBYF' },
   { name: 'KD Car Decal',       cat: 'khac',     type: 'traphi',  img: 'images/kdcardecal.jpg',      link: '1F3XH4LcatYqKFmHUG1dLq440cGeNzkXv' },
-  { name: 'KD Causten',         cat: 'khac',     type: 'mienphi', img: 'images/kdcausten.jpg',       link: '17ZWlzGNJHzT13oevTGvXeYN2JCgvzd0q' },
-  { name: 'KD Chatoyer',        cat: 'cuoi',     type: 'mienphi', img: 'images/kdchatoyer.jpg',      link: '14-ofEeBwK019Af30Uss2kBY3Q2eFigEk' },
-  { name: 'KD Midautumn',       cat: 'khac',     type: 'mienphi', img: 'images/kdmidautumn.jpg',     link: '1b-Xqg_Pvzc944VGQMKBukumQeriznL2F' },
+  { name: 'KD Causten',         cat: 'khac',     type: 'mienphi', img: 'images/kdcausten.jpg',       link: '17ZWlzGNJHzT13oevTGvXeYN2JCgvzd0q', vipLink: '1dFMeDlwWIQeGA_XbFdWnXDa-rDkveAXI' },
+  { name: 'KD Chatoyer',        cat: 'cuoi',     type: 'mienphi', img: 'images/kdchatoyer.jpg',      link: '14-ofEeBwK019Af30Uss2kBY3Q2eFigEk', vipLink: '16M9VCWGTkw37drJ03RLlAa38UaLAlUG8' },
+  { name: 'KD Midautumn',       cat: 'khac',     type: 'mienphi', img: 'images/kdmidautumn.jpg',     link: '1b-Xqg_Pvzc944VGQMKBukumQeriznL2F', vipLink: '1IjMeZ8rwVExUli_O7aPLA6NHhKSpz1zN' },
   { name: 'KD Cute Tattoo Girl',cat: 'khac',     type: 'traphi',  img: 'images/kdcutetattoogirl.jpg',link: '1AIXtW74xODWeAnyJch78bPO9h9d_X8LN' },
   { name: 'KD Estarossa',       cat: 'cuoi',     type: 'traphi',  img: 'images/kdestarossa.jpg',     link: '1taLfNWkBPvaN4aCy5MvKJZ_kl0fMIo4V' },
-  { name: 'KD Fallen Bishop',   cat: 'khac',     type: 'mienphi', img: 'images/kdfallenbishop.jpg',  link: '1eIJy2cWadriEYI4EJMgrEDYCi4DagNeq' },
-  { name: 'KD Fashionwacks',    cat: 'khac',     type: 'mienphi', img: 'images/kdfashionwacks.jpg',  link: '1Fa8I2Q47PK-_KPLgTZ4PY6hkn8mIfF_t' },
+  { name: 'KD Fallen Bishop',   cat: 'khac',     type: 'mienphi', img: 'images/kdfallenbishop.jpg',  link: '1eIJy2cWadriEYI4EJMgrEDYCi4DagNeq', vipLink: '1NDFNOhdPF7dwWearj9SePWaorniwg62e' },
+  { name: 'KD Fashionwacks',    cat: 'khac',     type: 'mienphi', img: 'images/kdfashionwacks.jpg',  link: '1Fa8I2Q47PK-_KPLgTZ4PY6hkn8mIfF_t', vipLink: '12YGf6vZqpJ0RZGTmWplJwTwkUgH73P0n' },
   { name: 'KD Fit',             cat: 'khac',     type: 'traphi',  img: 'images/kdfit.jpg',           link: '1hJa9iQ0KvASIqMjtNYWXbXX0ffOKooxL' },
   { name: 'KD Flourshing',      cat: 'cuoi',     type: 'traphi',  img: 'images/kdflourshing.jpg',    link: '1rJHf9CYUYVwn5MtxtGc06qwq6nCDPNy6' },
-  { name: 'KD Gold Queen',      cat: 'khac',     type: 'mienphi', img: 'images/kdgoldqueen.jpg',     link: '16h_3idNiGUhwJ2Swtwgy-fPXy9GKg4KI' },
-  { name: 'KD Goodrace',        cat: 'khac',     type: 'mienphi', img: 'images/kdgoodrace.jpg',      link: '1S-fM8OmhsudF9ZadwxXvB7-Rekp-31Cn' },
-  { name: 'KD Hangout',         cat: 'khac',     type: 'mienphi', img: 'images/kdhangout.jpg',       link: '1CzHypN4jFVGu-toCbJtmf5_AxpI5cQw1' },
+  { name: 'KD Gold Queen',      cat: 'khac',     type: 'mienphi', img: 'images/kdgoldqueen.jpg',     link: '16h_3idNiGUhwJ2Swtwgy-fPXy9GKg4KI', vipLink: '1SP2SOVgGPBSLhPtSsfz30GitpjA0jsZs' },
+  { name: 'KD Goodrace',        cat: 'khac',     type: 'mienphi', img: 'images/kdgoodrace.jpg',      link: '1S-fM8OmhsudF9ZadwxXvB7-Rekp-31Cn', vipLink: '1rF-zSeOdEGe5EIHLBhlx3kxYN7XHBlAd' },
+  { name: 'KD Hangout',         cat: 'khac',     type: 'mienphi', img: 'images/kdhangout.jpg',       link: '1CzHypN4jFVGu-toCbJtmf5_AxpI5cQw1', vipLink: '1_Rq3QFZDf5mSM1C2vQyRxpeyyEUMUaDu' },
   { name: 'KD Holyriver',       cat: 'cuoi',     type: 'traphi',  img: 'images/kdholyriver.jpg',     link: '1dBGceeZkryTXwlzRFjcUnmhjT4W-6ruX' },
   { name: 'KD Lordish',         cat: 'khac',     type: 'traphi',  img: 'images/kdlordish.jpg',       link: '1rcVviMPvJMJRbMNeqMPjgBfvbq5Iqzq7' },
   { name: 'KD Love Lost',       cat: 'khac',     type: 'traphi',  img: 'images/kdlovelost.jpg',      link: '19C_DJvwZAG2yuMRNcAy1llWmdfDRZqjl' },
   { name: 'KD Luxhouse',        cat: 'khac',     type: 'traphi',  img: 'images/kdluxhouse.jpg',      link: '1N-NoV3X4Kg3-NoSfS0C8kHTvDJzdtoyx' },
-  { name: 'KD Quinzmom',        cat: 'khac',     type: 'mienphi', img: 'images/kdquinzmom.jpg',      link: '1sNTeYVuIgy_PkPrPkCSGCNngHo9tDUO3' },
+  { name: 'KD Quinzmom',        cat: 'khac',     type: 'mienphi', img: 'images/kdquinzmom.jpg',      link: '1sNTeYVuIgy_PkPrPkCSGCNngHo9tDUO3', vipLink: '1oEaFG85Hcf8waNQV-fMX_i-vUAO60hkN' },
   { name: 'KD Quynh',           cat: 'cuoi',     type: 'traphi',  img: 'images/kdquynh.jpg',         link: '1bNsvC7iXF_XIoR17WwyptFia_nzVOoK3' },
   { name: 'KD Siren',           cat: 'cuoi',     type: 'traphi',  img: 'images/kdsiren.jpg',         link: '1xA6w2mhC1K-1tCQUPjGcBPDpbdWWXUPP' },
   { name: 'KD Suorva',          cat: 'cuoi',     type: 'traphi',  img: 'images/kdsuorva.jpg',        link: '1HyX3AIXb8pGmh0fF4nUJxKlQfItDcQug' },
   { name: 'KD Tan Headline',    cat: 'khac',     type: 'traphi',  img: 'images/kdtanheadline.jpg',   link: '1Z-1Z5O0soU0yB66gBWAnPnZAZ99Ua1QM' },
   { name: 'KD The Blanger',     cat: 'cuoi',     type: 'traphi',  img: 'images/kdtheblanger.jpg',    link: '1SFByl3b4cfNKKDKGRIpynaKbb3QPO_wd' },
-  { name: 'KD Vince Display',   cat: 'khac',     type: 'mienphi', img: 'images/kdvincedisplay.jpg',  link: '1b6KN36utlHcZqc2-Mi8NRHdWz4SfvIgs' },
-  { name: 'KD Yon Love',        cat: 'khac',     type: 'mienphi', img: 'images/kdyonlove.jpg',       link: '1CkgxirVfbSgkaMRLaXk9gWmrHsKqNMzS' },
-  { name: 'KD Zoika',           cat: 'khac',     type: 'mienphi', img: 'images/kdzoika.jpg',         link: '1tVxUfTcBy3l1jJoNRcdVQkaQkhYMGgSs' },
-  
+  { name: 'KD Vince Display',   cat: 'khac',     type: 'mienphi', img: 'images/kdvincedisplay.jpg',  link: '1b6KN36utlHcZqc2-Mi8NRHdWz4SfvIgs', vipLink: '1HhGH01C5J2DBE1u3omnU05ZASWTWjqnV' },
+  { name: 'KD Yon Love',        cat: 'khac',     type: 'mienphi', img: 'images/kdyonlove.jpg',       link: '1CkgxirVfbSgkaMRLaXk9gWmrHsKqNMzS', vipLink: '1XZFhCHq8-AlD8lXz-0gm75dgaLghgBNO' },
+  { name: 'KD Zoika',           cat: 'khac',     type: 'mienphi', img: 'images/kdzoika.jpg',         link: '1tVxUfTcBy3l1jJoNRcdVQkaQkhYMGgSs', vipLink: '1Wr-z6rlpSaHNB61upTxzHtU5vmgrLC15' },
   { name: 'Bộ KD',     cat: 'kdvip', type: 'kdvip', img: 'images/kd.jpg',   link: '1vgcFX7IAhI6HOyq5C3oI0mY7f8LZHkwE', updateDate: 'Cập nhật 16/05/26' },
   { name: 'Bộ 1FTV',   cat: 'kdvip', type: 'kdvip', img: 'images/1ftv.jpg', link: '1aIiSUrC8-ry-xF5WxSjJAwaM76A5Ay0R', updateDate: 'Cập nhật 16/05/26' },
   { name: 'Bộ BHN',    cat: 'kdvip', type: 'kdvip', img: 'images/bhn.jpg',  link: '1JDIGGYLPHDhefeRgjwA1EFLqIhonRxaF', updateDate: 'Cập nhật 15/06/26' },
@@ -81,6 +80,7 @@ const allFonts = [
   { name: 'Bộ VNI',    cat: 'kdvip', type: 'kdvip', img: 'images/vni.jpg',  link: '1T_iFeuONTDQvZmEnMYNTn0gDCFAR2iou', updateDate: 'Cập nhật 16/05/26' },
 ];
 
+let freeCountdownActive = false;
 const iconCart = `<i class="ri-download-cloud-2-line"></i>`;
 const iconDl = `<i class="ri-download-cloud-2-line"></i>`;
 
@@ -89,7 +89,46 @@ let currentCat = 'all';
 let filtered = [];
 let shown = 0;
 
+const FREE_WAIT = 60;
+
+function startFreeCountdown(btn, url) {
+  if (freeCountdownActive) {
+    alert('Vui lòng chờ hết 60s để tải font khác. \nĐăng ký VIP để tải không giới hạn.');
+    return;
+  }
+
+  freeCountdownActive = true;
+  let remaining = FREE_WAIT;
+  btn.disabled = true;
+  btn.dataset.originalHtml = btn.innerHTML;
+
+  function tick() {
+    btn.innerHTML = `<i class="ri-time-line"></i> ${remaining}s`;
+    if (remaining <= 0) {
+      clearInterval(timer);
+      triggerDownload(url);
+      btn.innerHTML = btn.dataset.originalHtml;
+      btn.disabled = false;
+      freeCountdownActive = false; // Mở khoá cho font khác
+    }
+    remaining--;
+  }
+
+  tick();
+  const timer = setInterval(tick, 1000);
+}
+
+function triggerDownload(url) {
+  const a = document.createElement('a');
+  a.href = url;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+// ============================================================
 // Search
+// ============================================================
 function onSearch() {
   const val = document.getElementById('searchInput').value;
   document.getElementById('clearBtn').classList.toggle('visible', val.length > 0);
@@ -106,9 +145,8 @@ function setChip(el, cat) {
   document.querySelectorAll('.chip').forEach(c => c.classList.remove('active'));
   el.classList.add('active');
   currentCat = cat;
-   document.getElementById('searchInput').value = '';
+  document.getElementById('searchInput').value = '';
   document.getElementById('clearBtn').classList.remove('visible');
-  
   runFilter();
 }
 
@@ -147,43 +185,59 @@ function runFilter() {
   }
 }
 
+// ============================================================
 // Card HTML
+// ============================================================
 function cardHTML(f) {
   const isFree = f.type === 'mienphi';
   const isVip = f.type === 'kdvip';
   const iconMore = `<i class="ri-image-line"></i>`;
-  
+
   const resolvedLink = (f.link && f.link !== '#' && !f.link.startsWith('http'))
     ? `https://drive.google.com/uc?export=download&id=${f.link}`
     : f.link;
 
-    const isPreview = f.type === 'preview';
+    const resolvedVipLink = f.vipLink
+  ? (f.vipLink.startsWith('http') ? f.vipLink : `https://drive.google.com/uc?export=download&id=${f.vipLink}`)
+  : resolvedLink;
 
-    const priceHTML = isFree
+  const isPreview = f.type === 'preview';
+
+  const priceHTML = isFree
     ? `<span class="price free">FREE</span>`
     : isVip
-        ? `<span></span>`
+      ? `<span></span>`
       : isPreview
         ? `<span class="price">Liên hệ</span>`
         : `<span class="price vip">VIP</span>`;
-    
-        const btnHTML = isFree
-        ? `<button class="action-btn dl" onclick="downloadFont(this,'${resolvedLink}')">${iconDl} Tải font</button>`
-        : isVip
-          ? `<button class="action-btn dl" onclick="window.open('${resolvedLink}','_blank')">${iconDl} Tải về</button>`
-          : isPreview
-          ? `<button class="action-btn dl" onclick='openSlideModal(${JSON.stringify(f.slides)})'>${iconMore} Xem thêm</button>`
-          : `<button class="action-btn buy" onclick="downloadFont(this,'${resolvedLink}')">${iconCart} Tải font</button>`;
-    const updateBadge = isVip && f.updateDate
-        ? `<span class="update-badge">
-         ${f.updateDate}
-        </span>`
-        : '';
-    const styleBadge = f.style
-      ? `<span class="style-badge">
-       ${f.style}
-      </span>`
-        : '';
+
+  let btnHTML;
+  if (isFree) {
+
+    btnHTML = `
+      <div class="btn-group-free">
+        <button class="action-btn dl free-dl-btn" onclick="startFreeCountdown(this,'${resolvedLink}')">
+          ${iconDl} Free
+        </button>
+        <button class="action-btn buy vip-skip-btn" onclick="downloadFont(this,'${resolvedVipLink}')" title="VIP – tải ngay không cần chờ">
+  ${iconCart} VIP
+        </button>
+      </div>`;
+  } else if (isVip) {
+    btnHTML = `<button class="action-btn dl" onclick="window.open('${resolvedLink}','_blank')">${iconDl} Tải về</button>`;
+  } else if (isPreview) {
+    btnHTML = `<button class="action-btn dl" onclick='openSlideModal(${JSON.stringify(f.slides)})'>${iconMore} Xem thêm</button>`;
+  } else {
+
+    btnHTML = `<button class="action-btn buy" onclick="downloadFont(this,'${resolvedLink}')">${iconCart} Tải VIP</button>`;
+  }
+
+  const updateBadge = isVip && f.updateDate
+    ? `<span class="update-badge">${f.updateDate}</span>`
+    : '';
+  const styleBadge = f.style
+    ? `<span class="style-badge">${f.style}</span>`
+    : '';
 
   return `
     <div class="card">
@@ -231,16 +285,13 @@ function loadMore() {
   appendCards();
 }
 
+// downloadFont dùng cho VIP (không đếm ngược)
 function downloadFont(btn, url) {
   const original = btn.innerHTML;
   btn.innerHTML = '<i class="ri-loader-4-line ri-spin"></i> Đang tải...';
   btn.disabled = true;
 
-  const a = document.createElement('a');
-  a.href = url;
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+  triggerDownload(url);
 
   function onBlur() {
     btn.innerHTML = original;
@@ -255,6 +306,7 @@ function downloadFont(btn, url) {
     btn.disabled = false;
   }, 5000);
 }
+
 runFilter();
 
 const targetFont = decodeURIComponent(window.location.hash.slice(1));
@@ -269,11 +321,11 @@ if (targetFont) {
 const backToTopBtn = document.querySelector('.backtotop-btn');
 
 window.addEventListener('scroll', () => {
-    backToTopBtn.style.display = window.scrollY > 1000 ? 'flex' : 'none';
+  backToTopBtn.style.display = window.scrollY > 1000 ? 'flex' : 'none';
 });
 
 backToTopBtn.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
 
